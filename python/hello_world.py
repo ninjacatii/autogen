20ebe987f30fc78f92320a1c78472e4e28d71e46
Kross <2813402@qq.com>
2025-03-16 22:16:05 +0800

    111

---
+++ python/hello_world.py
@@ -0,0 +1,14 @@
+import asyncio
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+async def main() -> None:
+    model_client = OpenAIChatCompletionClient(
+        model="gemini-1.5-flash-8b",
+    )
+    
+    agent = AssistantAgent("assistant", model_client=model_client)
+    print(await agent.run(task="Say 'Hello World!'"))
+    await model_client.close()
+
+asyncio.run(main())

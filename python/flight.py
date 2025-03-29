419f36837c3f6003ef4554c19e9ade7d3c4a7ac4
Kross <2813402@qq.com>
2025-03-29 17:51:00 +0800

    111

--- python/flight.py
+++ python/flight.py
@@ -9,6 +9,12 @@ from autogen_agentchat.teams import Swarm
 from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
+def query_price(airplane: str) -> str:
+    return "The price is $1234"
+
+def query_flight(date: str) -> str: 
+    return f"{date}: Boeing 737 airplane"
+
 def refund_flight(flight_id: str) -> str:
     """Refund a flight"""
     return f"Flight {flight_id} refunded"
@@ -25,11 +31,37 @@ model_client = OpenAIChatCompletionClient(
 travel_agent = AssistantAgent(
     "travel_agent",
     model_client=model_client,
-    handoffs=["flights_refunder", "user"],
+    handoffs=["flights_refunder", "flights_queryer", "flights_pricer", "user"],
     system_message="""You are a travel agent.
     The flights_refunder is in charge of refunding flights.
+    The flights_queryer is in charge of querying flights.
+    The flights_pricer is in charge of querying price of airplane.
     If you need information from the user, you must first send your message, then you can handoff to the user.
     Use TERMINATE when the travel planning is complete.""",
+)
+
+flights_pricer = AssistantAgent(
+    "flights_pricer",
+    model_client=model_client,
+    handoffs=["travel_agent", "user"],
+    tools=[query_price],
+    system_message="""You are an agent specialized in querying the price of airplane.
+    You only need the airplane type to query price.
+    You have the ability to query price of airplane type using the query_price tool.
+    If you need information from the user, you must first send your message, then you can handoff to the user.
+    When the query is complete, handoff to the travel agent to finalize.""",
+)
+
+flights_queryer = AssistantAgent(
+    "flights_queryer",
+    model_client=model_client,
+    handoffs=["travel_agent", "user"],
+    tools=[query_flight],
+    system_message="""You are an agent specialized in querying flights.
+    You only need the date of flight to query a flight.
+    You have the ability to query a flight using the query_flight tool.
+    If you need information from the user, you must first send your message, then you can handoff to the user.
+    When the query is complete, handoff to the travel agent to finalize.""",
 )
 
 flights_refunder = AssistantAgent(
@@ -45,9 +77,9 @@ flights_refunder = AssistantAgent(
 )
 
 termination = HandoffTermination(target="user") | TextMentionTermination("TERMINATE")
-team = Swarm([travel_agent, flights_refunder], termination_condition=termination)
+team = Swarm([travel_agent, flights_refunder, flights_queryer, flights_pricer], termination_condition=termination)
 
-task = "I need to refund my flight."
+task = "I need to query flight."
 
 async def main() -> None:
     async def run_team_stream() -> None:
